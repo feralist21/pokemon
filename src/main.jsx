ec44751d429a5241baf9cdc98c473b674e2f3c9f
Alexandr Dementev <ad@adictgroup.ru>
2024-10-11 15:02:38 +0800

    Добавляет PrimeReact

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { PrimeReactProvider } from 'primereact/api';
 import App from './App.jsx';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <PrimeReactProvider>
+            <App />
+        </PrimeReactProvider>
     </StrictMode>,
 );
