df777d8ad0ce5cbbc6e7cf0133a7de6bf4dc11c5
Alexandr Dementev <feralist2111@yandex.ru>
2024-10-21 19:34:51 +0800

    Добавляет базовый роутинг

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,16 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { PrimeReactProvider } from 'primereact/api';
 import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
-import { HomePage } from '@/pages/homePage';
+import { Favorite } from '@/pages/favorite';
+import { HomePage } from '@/pages/home';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <PrimeReactProvider>
-            <HomePage />
+            <Router>
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='/favorite' element={<Favorite />} />
+                </Routes>
+            </Router>
         </PrimeReactProvider>
     </StrictMode>,
 );
---
+++ src/pages/favorite/ui/favorite.jsx
@@ -0,0 +1,5 @@
+const Favorite = () => {
+    return <div>favorite page</div>;
+};
+
+export { Favorite };
