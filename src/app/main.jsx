203e77722234ce36a27a104791b2b349e78cf366
Alexandr Dementev <feralist2111@yandex.ru>
2024-10-21 19:25:38 +0800

    Удаляет лишнее и начинает заново

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { PrimeReactProvider } from 'primereact/api';
-import App from './App.jsx';
 import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
+import { HomePage } from '@/pages/homePage';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <PrimeReactProvider>
-            <App />
+            <HomePage />
         </PrimeReactProvider>
     </StrictMode>,
 );
