f736671809925862dc1b8d46c52295e0a8e2569c
Alexandr Dementev <feralist2111@yandex.ru>
2024-10-21 20:17:31 +0800

    Добавляет tailwind и базовый layout

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { PrimeReactProvider } from 'primereact/api';
-import 'primereact/resources/primereact.min.css';
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
 import { Favorite } from '@/pages/favorite';
 import { HomePage } from '@/pages/home';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
--- src/pages/home/ui/home.jsx
+++ src/pages/home/ui/home.jsx
@@ -1,5 +1,28 @@
 const HomePage = () => {
-    return <div>home page</div>;
+    return (
+        <div className='flex flex-col min-h-screen'>
+            <header className='py-6'>
+                <div className='container mx-auto px-6 flex items-center justify-between'>
+                    <a className='text-2xl font-semibold' href='/'>
+                        Pokedex
+                    </a>
+                    <div>избранное</div>
+                </div>
+            </header>
+            <main className='flex-grow'>
+                <div className='container mx-auto px-6'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis amet recusandae eaque perferendis
+                    tenetur? Ipsum porro eos tempore laboriosam fugiat nisi reprehenderit quibusdam officia,
+                    exercitationem debitis! Commodi eos sit aspernatur?
+                </div>
+            </main>
+            <footer className='py-10'>
+                <div className='container mx-auto px-6'>
+                    <p>footer</p>
+                </div>
+            </footer>
+        </div>
+    );
 };
 
 export { HomePage };
