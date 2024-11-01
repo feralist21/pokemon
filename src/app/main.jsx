8f17de848052204dec0d52428fdd29e6c2fdd951
Alexandr Dementev <ad@adictgroup.ru>
2024-11-01 09:59:24 +0800

    Добавил RTK

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,19 +2,23 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { PrimeReactProvider } from 'primereact/api';
+import { Provider } from 'react-redux';
 import { Favorite } from '@/pages/favorite';
 import { HomePage } from '@/pages/home';
+import { store } from '@/shared/store';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <PrimeReactProvider>
-            <Router>
-                <Routes>
-                    <Route path='/' element={<HomePage />} />
-                    <Route path='/favorite' element={<Favorite />} />
-                </Routes>
-            </Router>
-        </PrimeReactProvider>
+        <Provider store={store}>
+            <PrimeReactProvider>
+                <Router>
+                    <Routes>
+                        <Route path='/' element={<HomePage />} />
+                        <Route path='/favorite' element={<Favorite />} />
+                    </Routes>
+                </Router>
+            </PrimeReactProvider>
+        </Provider>
     </StrictMode>,
 );
