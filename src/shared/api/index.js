cf412989dce16aab260d29c3f400b2a088196aee
Alexandr Dementev <ad@adictgroup.ru>
2024-11-22 16:45:56 +0800

    Добавляет таймаут для запросов

--- src/shared/api/index.js
+++ src/shared/api/index.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const api = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/', timeout: 3000 }),
     endpoints: () => ({}),
 });
 
