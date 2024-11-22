07e9360e610537abfcf659783e31b9f11f5799c0
Alexandr Dementev <ad@adictgroup.ru>
2024-11-22 16:21:48 +0800

    Добавляет ссылки в шапку из router

--- src/widgets/LayoutHeader/ui/LayoutHeader.jsx
+++ src/widgets/LayoutHeader/ui/LayoutHeader.jsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const LayoutHeader = () => {
     return (
         <header className='py-6'>
             <div className='container mx-auto px-6 flex items-center justify-between'>
-                <a className='text-2xl font-semibold' href='/'>
+                <Link to='/' className='text-2xl font-semibold'>
                     Pokedex
-                </a>
-                <div>избранное</div>
+                </Link>
+                <Link to='favorite'>избранное</Link>
             </div>
         </header>
     );
